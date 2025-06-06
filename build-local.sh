bdc19951d02eaa95c16f75a97619d69b1ad8cc6a
Alexey Averikhin <alexey.averikhin@labs64.de>
2025-06-06 18:38:50 +0200

    Rename build-local.sh

---
+++ build-local.sh
@@ -0,0 +1,17 @@
+## Toolset
+# Local: k8s, kubectl, helm, docker, maven, git, java
+
+# Local docker repository
+# docker run -d -p 5005:5000 --name registry registry:2
+
+docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | grep "api-gateway" | awk '{print $2}' | xargs -r docker rmi -f
+
+# Build project
+mvn clean package
+
+# Build docker images
+docker build -t api-gateway:latest .
+docker tag api-gateway:latest localhost:5005/api-gateway:latest
+docker push localhost:5005/api-gateway:latest
+
+docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | grep "api-gateway"
